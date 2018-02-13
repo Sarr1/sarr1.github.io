3a3b8f3b9b185459a65243537f42099f9d067f8f
Sarri <sarritalhami@gmail.com>
2018-02-13 14:52:37 +1100

    asd

--- VRFACE/js/examples/face_tracking/ThreeJS_example.js
+++ VRFACE/js/examples/face_tracking/ThreeJS_example.js
@@ -12,7 +12,7 @@
 
 			t3d.removeAll();
 			t3d.loadOcclusionHead("assets/brfv4_occlusion_head.json", numFacesToTrack);
-			t3d.loadModel("assets/cv1_FF.json", numFacesToTrack);
+			t3d.loadModel("assets/oculus.json", numFacesToTrack);
 		}
 	}
 
